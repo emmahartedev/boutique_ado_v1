94e249fb6e17bd83f1455fecd23364a773f6778b
emmahartedev <emmaharte33@outlook.com>
2021-07-17 12:33:31 +0000

    Deploy part 5

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -175,7 +175,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 if 'USE_AWS' in os.environ:
     # bucket config
     AWS_STORAGE_BUCKET_NAME = 'emmahartedev-boutique-ado'
-    AWS_S3_REGION_NAME = 'EU (Paris) eu-west-3'
+    AWS_S3_REGION_NAME = 'eu-west-3'
     AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY_ID')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
