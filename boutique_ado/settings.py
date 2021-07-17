644e5f3c1b62ee5510c756114b5a770350168c88
emmahartedev <emmaharte33@outlook.com>
2021-07-17 10:25:31 +0000

    Deployment part 1

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -172,4 +173,4 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-DEFAULT_FROM_EMAIL = 'emmaharte33@outlook.com'
+DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
